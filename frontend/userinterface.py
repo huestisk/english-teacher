7b14561e972ea1a09d244e04a7b33f6906e6b74d
Kevin Huestis <huestiskevin19@gmail.com>
2020-12-07 21:19:07 +0100

    Improved UI

--- frontend/userinterface.py
+++ frontend/userinterface.py
@@ -1,14 +1,18 @@
 # class that creates a simple display to show the "emotion" of the robot
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.config import Config
+from kivy.uix.floatlayout import FloatLayout
 
 from frontend.voice_synthesis import say
 
-class ContainerBox(BoxLayout):
+Config.set('graphics', 'resizable', True) 
+
+
+class ContainerBox(FloatLayout):
     def speech_synthesis(self, *args):
         text = self.text_field.text
-        say(text)
+        if text:
+            say(text)
 
     def speech_recognition(self, *args):
         # TODO
